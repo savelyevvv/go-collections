51618f6542a4405a5ddaa61b983355cbf2142d62
Vladislav Savelyev <BashWhiteShaman@yandex.com>
2022-12-16 14:33:18 +0700

    Add HashTable structure, initializer and String() method

--- ht/ht.go
+++ ht/ht.go
@@ -1,6 +1,11 @@
 package ht
 
-import "github.com/savelyevvv/go-collections/sll"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/savelyevvv/go-collections/sll"
+)
 
 // HashTable
 // .Insert
@@ -10,14 +15,28 @@ import "github.com/savelyevvv/go-collections/sll"
 // Hash()
 
 type HashTable[E any] struct {
-	buckets []sll.List[E]
+	buckets []*sll.List[E]
 	size    int
 }
 
+func New[E any](size int) *HashTable[E] {
+	t := &HashTable[E]{
+		buckets: make([]*sll.List[E], size),
+	}
+	for i := range t.buckets {
+		t.buckets[i] = sll.New[E]()
+	}
+	return t
+}
+
 func (t *HashTable[E]) String() string {
+	ss := make([]string, 0, t.size)
 	for _, bucket := range t.buckets {
 		if bucket.Size() > 0 {
-
+			for _, element := range bucket.ToSlice() {
+				ss = append(ss, fmt.Sprintf("%v", element))
+			}
 		}
 	}
+	return fmt.Sprintf("[%s]", strings.Join(ss, ","))
 }
--- ht/ht_test.go
+++ ht/ht_test.go
@@ -1,1 +1,10 @@
 package ht
+
+import "fmt"
+
+func ExampleNew() {
+	size := 5
+	table := New[int](size)
+	fmt.Println(table)
+	// Output: []
+}
